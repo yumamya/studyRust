bf67894adb87cdf3be03b09d22dcda6a2f5b912e
yumamya <yuyam4712@gmail.com>
2025-08-12 17:34:07 +0900

    数字当てクイズ　問題をランダム化

--- arithmetic-operations-quiz/src/main.rs
+++ arithmetic-operations-quiz/src/main.rs
@@ -1,5 +1,13 @@
+use rand::Rng;
+
 fn main() {
-    println!("1 + 1 = ??");
+    // 参考にしている技術書の記述ではバージョンが古い（0.8.5）なので、
+    // 最新のrand（0.9.2）を利用しているとここがwarningになる
+    let op1 = rand::rng().random_range(0..100);
+    let op2 = rand::rng().random_range(0..100);
+
+    // 加算
+    println!("{} + {} = ??", op1, op2);
     println!("?? の値を入力して下さい");
     let mut ans_input = String::new(); // ユーザからの回答を保持する変数
     // 標準入力から1行取得し、ans_input に代入する
@@ -9,13 +17,16 @@ fn main() {
     let ans_input = ans_input.trim().parse::<u32>().unwrap();
 
     dbg!(ans_input); // 実行後にキーボードで入力した値を確認できる
-    if ans_input == 1 + 1 {
+    if ans_input == op1 + op2 {
         println!("正解");
     } else {
         println!("不正解");
     }
 
-    println!("1 - 4 = ??");
+    // 減算
+    let op1 = rand::rng().random_range(0..100);
+    let op2 = rand::rng().random_range(0..100);
+    println!("{} - {} = ??", op1, op2);
     println!("?? の値を入力して下さい");
     let mut ans_input = String::new(); // ユーザからの回答を保持する変数
     // 標準入力から1行取得し、ans_input に代入する
@@ -25,7 +36,7 @@ fn main() {
     let ans_input = ans_input.trim().parse::<i32>().unwrap();
 
     dbg!(ans_input); // 実行後にキーボードで入力した値を確認できる
-    if ans_input == 1 - 4 {
+    if ans_input == op1 - op2 {
         println!("正解");
     } else {
         println!("不正解");
