762655f0889a9041d743b6788a805319542c9862
yumamya <yuyam4712@gmail.com>
2025-08-19 22:33:50 +0900

    メモリ機能付き電卓 初回コミット

---
+++ calculator-with-memory-function/src/main.rs
@@ -0,0 +1,28 @@
+use std::io::stdin;
+
+fn main() {
+    for line in stdin().lines() {
+        // 1行読み取って空行なら終了
+        let line = line.unwrap();
+        if line.is_empty() {
+            break;
+        }
+        // 空白で分割
+        let tokens: Vec<&str>= line.split(char::is_whitespace).collect();
+        // 式の計算
+        let left: f64 = tokens[0].parse().unwrap();
+        let right: f64 = tokens[2].parse().unwrap();
+        let result = match tokens[1] {
+            "+" => left + right,
+            "-" => left - right,
+            "*" => left * right,
+            "/" => left / right,
+            _ => {
+                // 入力が正しければここには到達しない
+                unreachable!()
+            }
+        };
+        // 結果表示
+        println!(" => {}", result);
+    }
+}
