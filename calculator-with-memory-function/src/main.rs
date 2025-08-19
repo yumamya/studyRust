02c07d3deef38dda05788b22e4db1727ccc67d15
yumamya <yuyam4712@gmail.com>
2025-08-19 22:57:45 +0900

    メモリ機能付き電卓 式の計算部分を関数に

--- calculator-with-memory-function/src/main.rs
+++ calculator-with-memory-function/src/main.rs
@@ -1,6 +1,9 @@
 use std::io::stdin;
 
 fn main() {
+    let mut memory: f64 = 0.0;
+    let mut prev_result: f64 = 0.0;
+
     for line in stdin().lines() {
         // 1行読み取って空行なら終了
         let line = line.unwrap();
@@ -9,25 +12,52 @@ fn main() {
         }
         // 空白で分割
         let tokens: Vec<&str>= line.split(char::is_whitespace).collect();
+
+        // メモリへの書き込み
+        if tokens[0] == "mem+" {
+            memory += prev_result;
+            print_output(memory);
+            continue;
+        } else if tokens[0] == "mem-" {
+            memory -= prev_result;
+            print_output(memory);
+            continue;
+        }
+
         // 式の計算
-        let left: f64 = tokens[0].parse().unwrap();
-        let right: f64 = tokens[2].parse().unwrap();
-        let result = match tokens[1] {
-            "+" => left + right,
-            "-" => left - right,
-            "*" => left * right,
-            "/" => left / right,
-            _ => {
-                // 入力が正しければここには到達しない
-                unreachable!()
-            }
-        };
+        let left = eval_token(tokens[0], memory);
+        let right = eval_token(tokens[2], memory);
+        let result = eval_expression(left, tokens[1], right);
+
         // 結果表示
         print_output(result);
+
+        prev_result = result;
     }
 }
 
 // 結果表示
 fn print_output(value: f64) {
     println!(" => {}", value);
+}
+
+fn eval_token(token: &str, memory: f64) -> f64 {
+    if token == "mem" {
+        memory
+    } else {
+        token.parse().unwrap()
+    }
+}
+
+fn eval_expression(left: f64, operator: &str, right: f64) -> f64 {
+    match operator {
+        "+" => left + right,
+        "-" => left - right,
+        "*" => left * right,
+        "/" => left / right,
+        _ => {
+            // 入力が正しければここには到達しない
+            unreachable!()
+        }
+    }
 }
