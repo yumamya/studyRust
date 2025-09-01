03345b97dccc47e21d623106352d08f007ae9ee8
yumamya <yuyam4712@gmail.com>
2025-09-01 23:31:16 +0900

    家計簿アプリ newコマンドの実装 複数口座を作れるようにする

--- kakeibo/src/main.rs
+++ kakeibo/src/main.rs
@@ -1,4 +1,5 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
+use csv::Writer;
 #[derive(Parser)]
 #[clap(version = "1.0")]
 struct App {
@@ -8,7 +9,7 @@ struct App {
 #[derive(Subcommand)]
 enum Command {
     /// 新しい口座を作る
-    New,
+    New(NewArgs),
     /// 口座に入金
     Deposit,
     /// 口座から出金
@@ -18,6 +19,34 @@ enum Command {
     /// レポート出力
     Report,
 }
-fn main() {
-    let _args = App::parse();
+#[derive(Args)]
+struct NewArgs {
+    account_name: String,
 }
+impl NewArgs {
+    fn run(&self) {
+        let file_name = format!("{}.csv", self.account_name);
+        let mut writer = Writer::from_path(file_name).unwrap();
+        writer.write_record(["日付", "用途", "金額"]).unwrap();
+        writer.flush().unwrap();
+    }
+}
+fn main() {
+    let args = App::parse();
+    match args.command {
+        Command::New(args) => args.run(),
+        Command::Deposit => unimplemented!(),
+        Command::Withdraw => unimplemented!(),
+        Command::Import => unimplemented!(),
+        Command::Report => unimplemented!(),
+    }
+}
+
+// fn new() {
+//     // accounts.csvというファイル名で作成
+//     let mut writer = Writer::from_path("accounts.csv").unwrap();
+//     writer
+//         .write_record(["日付", "用途", "金額"])
+//         .unwrap();
+//     writer.flush().unwrap();
+// }
