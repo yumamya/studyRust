86bef62facc68c36ddeeffd629f4b8a565f83845
yumamya <yuyam4712@gmail.com>
2025-09-01 23:12:16 +0900

    家計簿アプリ 家計簿機能のサブコマンド追加

--- kakeibo/src/main.rs
+++ kakeibo/src/main.rs
@@ -1,10 +1,23 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[clap(version = "1.0")]
-struct Args {
-    arg1: String,
-    arg2: String
+struct App {
+    #[clap(subcommand)]
+    command: Command,
+}
+#[derive(Subcommand)]
+enum Command {
+    /// 新しい口座を作る
+    New,
+    /// 口座に入金
+    Deposit,
+    /// 口座から出金
+    Withdraw,
+    /// CSVからインポート
+    Import,
+    /// レポート出力
+    Report,
 }
 fn main() {
-    let _args = Args::parse();
+    let _args = App::parse();
 }
