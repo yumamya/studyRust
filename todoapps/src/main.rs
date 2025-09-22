19031cad41a730dda80a6deede7baacf4df5ed1f
yumamya <yuyam4712@gmail.com>
2025-09-22 23:26:36 +0900

    ToDoアプリ helloメソッド getリクエストに応じて表示メッセージ変更

--- todoapps/src/main.rs
+++ todoapps/src/main.rs
@@ -1,7 +1,12 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 async fn greet() -> impl Responder {
     HttpResponse::Ok().body("hello world")
+}
+
+#[get("/hello/{name}")]
+async fn hello(name: web::Path<String>) -> String {
+    format!("Hello {name}!!")
 }
 
 #[actix_web::main]
@@ -9,6 +14,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
+            .service(hello)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
